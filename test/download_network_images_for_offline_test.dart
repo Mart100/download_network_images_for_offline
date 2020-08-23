0e994f906261e6a4611487e1fee2071d1b96a478
Marto_0 <martvanenck1@gmail.com>
2020-08-23 19:27:22 +0200

    added tests

--- test/download_network_images_for_offline_test.dart
+++ test/download_network_images_for_offline_test.dart
@@ -1,14 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:download_network_images_for_offline/download_network_images_for_offline.dart';
 
 void main() {
-  /*
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });*/
+  testWidgets('NetworkImageForOffline Runs', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ListView(
+            children: <Widget>[
+              NetworkImageForOffline(
+                file: File('./'),
+                url: 'https://i.imgur.com/5o2jOe0.png',
+                imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: imageProvider,
+                    ),
+                    title: Text('Test-Text'),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    final titleFinder = find.text('Test-Text');
+    final circleAvatarFinder = find.byType(CircleAvatar);
+    final networkImageForOfflineFinder = find.byType(NetworkImageForOffline);
+
+    expect(titleFinder, findsOneWidget);
+    expect(circleAvatarFinder, findsOneWidget);
+    expect(networkImageForOfflineFinder, findsOneWidget);
+  });
 }
